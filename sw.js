f243853310179e0877ad72952429a9b70cfed554
CodeSammich <7356670+CodeSammich@users.noreply.github.com>
2026-05-09 18:34:48 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
+// This version string is automatically synced from package.json during a PR
+// via GitHub actions via scripts/sync-version.js when creating or updating a new PR.
+// This controls a pop-up notification to users when a new version of the app is available for install
+// DO NOT EDIT THIS MANUALLY, as it will be overwritten by the next PR update.
+const VERSION = "0.8.6";
+
 self.addEventListener("install", (event) => {});
 
 self.addEventListener("activate", (event) => {
