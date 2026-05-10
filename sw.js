c50d223e2c8c9136e1f3f990f0e28a9ccfda4402
CodeSammich <7356670+CodeSammich@users.noreply.github.com>
2026-05-09 23:10:41 -0400

    Updates

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 // via GitHub actions via scripts/sync-version.js when creating or updating a new PR.
 // This controls a pop-up notification to users when a new version of the app is available for install
 // DO NOT EDIT THIS MANUALLY, as it will be overwritten by the next PR update.
-const VERSION = "0.9.2";
+const VERSION = "0.9.3";
+const CACHE_NAME = `sda-church-v${VERSION}`;
 
 self.addEventListener("install", (event) => {});
 
@@ -11,15 +12,33 @@ self.addEventListener("activate", (event) => {
     Promise.all([
       self.clients.claim(),
       // Force clear any browser-level caches that might be holding onto old versions of index.html or JS bundles.
-      caches
-        .keys()
-        .then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+      caches.keys().then((keys) => {
+        return Promise.all(
+          keys.map((key) => {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+          }),
+        );
+      }),
     ]),
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  // This empty fetch handler is the minimum requirement for PWA installation in Chrome.
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const resClone = response.clone();
+        caches
+          .open(CACHE_NAME)
+          .then((cache) => cache.put(event.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
 });
 
 self.addEventListener("message", (event) => {
