3b6b7f9a04691048ea09279947addbb9637be0ce
CodeSammich <7356670+CodeSammich@users.noreply.github.com>
2026-05-09 21:56:06 -0400

    Updates

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // via GitHub actions via scripts/sync-version.js when creating or updating a new PR.
 // This controls a pop-up notification to users when a new version of the app is available for install
 // DO NOT EDIT THIS MANUALLY, as it will be overwritten by the next PR update.
-const VERSION = "0.9.15";
+const VERSION = "0.9.16";
 
 self.addEventListener("install", (event) => {});
 
